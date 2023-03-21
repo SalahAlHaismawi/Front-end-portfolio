32ff2a4db33dfe2e2fccfe42df309c7d8732f1a3
Salah AH <salah322s1@gmail.com>
2023-03-21 09:55:55 +0800

    finishing contact section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,27 +1,55 @@
+/* eslint-disable react/jsx-key */
 import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 const Projects = (props: Props) => {
-    const projects = [1,2,3,4,5];
-  return <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
-    <h3 className="absolute top-24 uppercase tracking-[20px] text-2xl">Projects</h3>
-<div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
-{projects.map((project) =>(
-    <div>
-        <img />
-    </div>
-))}
-</div>
+  const projects = [1, 2, 3, 4, 5];
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-2xl">
+        Projects
+      </h3>
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project, i) => (
+          <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+            <motion.img
+              initial={{
+                y: -300,
+                opacity: 0,
+              }}
+              transition={{ duration: 1.2 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              src="https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image.png"
+            />
+            <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+              <h4 className="text-4xl font-semibold text-center">
+                <span className="underline">
+                  Case Study {i + 1} of {projects.length}
+                </span>{" "}
+                ups clone
+              </h4>
+              <p className="text-lg text-center md:text-left">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum
+                accusantium tenetur hic dolor, maxime sequi rem sunt ipsam omnis
+                consequuntur magni itaque repellat nulla ipsum iusto veritatis,
+                laborum aperiam alias.
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
 
-
-
-
-
-
-
-    <div className="w-full absolute top-[30%] bg-[#66fcf1]/20 left-0 h-[500px] -skew-y-12" />
-  </div>;
+      <div className="w-full absolute top-[30%] bg-[#66fcf1]/20 left-0 h-[500px] -skew-y-12" />
+    </motion.div>
+  );
 };
 
 export default Projects;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Header from '../../components/Header';
 import Hero from '../../components/Hero';
 import Projects from '../../components/Projects';
 import Skills from '../../components/Skills';
+import ContactMe from '../../components/ContactMe';
 
 
 
@@ -41,6 +42,9 @@ export default function Home() {
           <Projects />
         </section>
       {/* Contact me */}
+      <section id='contact' className='snap-start'>
+        <ContactMe />
+      </section>
       </div>   
   );
 };

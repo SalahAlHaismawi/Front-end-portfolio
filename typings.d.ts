c1f8a07f6b2a1fc1697467bbcc9e50d0b00953a3
Salah AH <salah322s1@gmail.com>
2023-05-13 05:28:05 +0800

    .

---
+++ typings.d.ts
@@ -0,0 +1,14 @@
+interface SanityBody {
+    _createdAt: string;
+    _id: string;
+    _rev: string;
+    _type: string;
+    _updatedAt: string;
+
+}
+
+export interface Social extends SanityBody {
+    _type: "social";,
+    title: string;,
+    url: string;,
+} 

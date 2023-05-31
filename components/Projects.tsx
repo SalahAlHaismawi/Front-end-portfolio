ab635e5785f415f6327662b4929541f0f75bf27e
Salah AH <77612931+SalahAlHaismawi@users.noreply.github.com>
2023-05-31 19:50:51 +0800

    .

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,8 +3,10 @@
 import Image from 'next/image';
 
 import React from "react";
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
 import website1 from '../assets/website1.png'
+import website2 from '../assets/website2.png'
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -13,24 +15,29 @@ import 'swiper/css/scrollbar';
 
 type Props = {};
 
+SwiperCore.use([Autoplay])
+SwiperCore.use([Navigation])
+
 const Projects = (props: Props) => {
   return (
     
-    <div className="h-screen flex flex-col items-center justify-center overflow-hidden " >
+    <div className="h-screen flex flex-col items-center justify-center overflow-hidden relative " >
+      <h3 className="absolute top-24  uppercase tracking-[20px] text-[#66fcf1] text-2xl">
+        Projects
+      </h3>
       <div className="max-w-4xl w-full">
         
       </div>
       <Swiper
         className="w-full"
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+        autoplay={{ 
+          disableOnInteraction: false,
+          delay: 5000 
+        }}
         spaceBetween={50}
         slidesPerView={1}
-        navigation
-        pagination={{
-          clickable: true,
-          bulletClass: 'custom-pagination-bullet', // Add custom class name for pagination bullet
-        }}
-        scrollbar={{ draggable: true }}
+    
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log('slide change')}
       >
@@ -46,14 +53,14 @@ const Projects = (props: Props) => {
         </SwiperSlide>
         <SwiperSlide className="flex items-center justify-center">
           <div className="flex justify-center">
-            <img
-              src='https://i.imgur.com/jVFgAKu.png'
+            <Image
+              src={website2}
               alt="Image 1"
               className="object-center w-full h-auto max-w-lg xl:max-w-3xl 2xl:max-w-4xl"
             />
           </div>
         </SwiperSlide>
-        <SwiperSlide className="flex items-center justify-center">
+        {/* <SwiperSlide className="flex items-center justify-center">
           <div className="flex justify-center">
             <img
               src='https://i.imgur.com/jVFgAKu.png'
@@ -61,8 +68,8 @@ const Projects = (props: Props) => {
               className="object-center w-full h-auto max-w-lg xl:max-w-3xl 2xl:max-w-4xl"
             />
           </div>
-        </SwiperSlide>
-        <SwiperSlide className="flex items-center justify-center">
+        </SwiperSlide> */}
+        {/* <SwiperSlide className="flex items-center justify-center">
           <div className="flex justify-center">
             <img
               src='https://i.imgur.com/jVFgAKu.png'
@@ -70,7 +77,10 @@ const Projects = (props: Props) => {
               className="object-center w-full h-auto max-w-lg xl:max-w-3xl 2xl:max-w-4xl"
             />
           </div>
-        </SwiperSlide>
+        </SwiperSlide> */}
+        <div className='custom-navigation'>
+
+        </div>
       </Swiper>
     </div>
   );

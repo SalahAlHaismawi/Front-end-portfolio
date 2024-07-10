76a90787d0343ee76b7f5343c4fa618843645101
Salah AH <77612931+SalahAlHaismawi@users.noreply.github.com>
2024-07-10 22:59:22 +0800

    p

--- components/About.tsx
+++ components/About.tsx
@@ -29,7 +29,7 @@ export default function About({}: Props) {
     {/*/>*/}
       <div className=" space-y-10 px-0 md:px-10">
         <p className="text-base text-[#eee2dc] text-center">
-          Hello! I'm Salah Fayeq Alhaismawi, a dedicated Full-Stack Developer and AI Enthusiast. With a passion for creating dynamic and user-friendly websites and applications, I specialize in building seamless full-stack solutions for both web and mobile platforms. My expertise spans across the latest technologies and frameworks, enabling me to craft robust and scalable applications.
+          Hello! Im Salah Fayeq Alhaismawi, a dedicated Full-Stack Developer and AI Enthusiast. With a passion for creating dynamic and user-friendly websites and applications, I specialize in building seamless full-stack solutions for both web and mobile platforms. My expertise spans across the latest technologies and frameworks, enabling me to craft robust and scalable applications.
             Driven by my enthusiasm for artificial intelligence, I am committed to integrating AI into practical and accessible software solutions. Whether it's developing sophisticated web applications or enhancing mobile user experiences, my goal is to leverage the power of AI to solve real-world problems and make technology more intuitive and impactful for users.
         </p>
       </div>

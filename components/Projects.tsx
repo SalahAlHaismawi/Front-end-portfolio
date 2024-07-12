7f0c08c646215825b03f4d44e963dc4005e896bb
Salah AH <77612931+SalahAlHaismawi@users.noreply.github.com>
2024-07-12 18:00:55 +0800

    test push

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,12 +1,8 @@
-/// <reference path="./react-slick.d.ts" />
-/* eslint-disable @next/next/no-img-element */
-/* eslint-disable react/jsx-key */
-/* tslint:disable */
 import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { client } from '../contentful'; // Ensure this path is correct and client is properly configured
+import { client } from '../contentful';
 
 type Project = {
   name: string;
@@ -38,40 +34,29 @@ const Projects = () => {
   }, []);
 
   const cardSettings = {
-    dots: true, // Enable pagination for card slider
-    arrows: false, // Disable arrows for the card slider
+    dots: true,
+    arrows: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
     centerMode: true,
-    centerPadding: '0px', // Set to 0px to prevent overflow
+    centerPadding: '60px',
+    focusOnSelect: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: true,
-          centerPadding: '0px',
+          slidesToShow: 2,
+          centerPadding: '40px',
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          centerMode: true,
-          centerPadding: '0px',
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: true,
-          centerPadding: '0px',
+          centerPadding: '20px',
         },
       },
     ],
@@ -79,7 +64,7 @@ const Projects = () => {
 
   const imageSettings = {
     dots: false,
-    arrows: true, // Enable arrows for the image slider
+    arrows: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
@@ -92,30 +77,29 @@ const Projects = () => {
         <h3 className=' absolute top-36 text-xs text-center underline'>Swipe To Explore Projects.</h3>
 
         <div className='w-[90%] h-[85%] flex flex-col items-center justify-center overflow-hidden pt-16'>
-
           <Slider {...cardSettings} className="w-full h-full">
             {projects.map((project, index) => (
-                <div key={index} className="w-full h-full">
-
+                <div key={index} className="p-2 scale-90 transition duration-300 ease-in-out hover:scale-100">
                   <div
-                      className="bg-[#1F2833] h-full rounded-xl p-6 flex flex-col justify-between items-center shadow-lg">
+                      className="bg-[#1F2833] p-6 flex flex-col justify-between items-center shadow-lg rounded-xl"
+                      style={{ maxWidth: '800px', width: '100%', margin: '0 10px' }}>
                     <h3 className="text-2xl font-bold text-[#66fcf1] mb-4">{project.name}</h3>
-                    <Slider {...imageSettings} className="w-full h-full mb-4">
+                    <Slider {...imageSettings} className="w-[95%] mb-4 p-4">
                       {project.images && project.images.length > 0 ? project.images.map((image, imgIndex) => (
                           <div key={imgIndex}
-                               className="w-full h-full flex items-center justify-center overflow-hidden">
+                               className="flex items-center justify-center overflow-hidden">
                             <img src={image} alt={`Project ${index + 1} Image ${imgIndex + 1}`}
-                                 className="rounded-xl object-contain w-full h-full max-w-[800px] mx-auto"/>
+                                 className="rounded-xl object-contain mx-auto"/>
                           </div>
                       )) : <p>No images available</p>}
                     </Slider>
-                    <div className="flex flex-col items-center">
-                      <div className="mb-4">
-                        <p className="text-md text-left">{project.description}</p>
-                      </div>
-                      <div className='w-full'>
-                        <h1 className='text-left font-bold text-sm pb-2'>Tech Stack:</h1>
-                        <div className="grid grid-cols-5 gap-5 justify-evenly w-full">
+                    <div className="flex flex-col items-center w-full">
+                      <p className="mb-4 text-sm" style={{hyphens: 'auto'}} lang="en">
+                        {project.description}
+                      </p>
+                      <div>
+                        <h1 className='text-left font-bold pb-2'>Tech Stack:</h1>
+                        <div className="grid grid-cols-5 gap-5">
                           {project.techStack && project.techStack.length > 0 ? project.techStack.map((tech, techIndex) => (
                               <img key={techIndex} src={tech} alt="Tech Stack Icon" className="h-10 w-10 rounded-full"/>
                           )) : <p>No tech stack available</p>}

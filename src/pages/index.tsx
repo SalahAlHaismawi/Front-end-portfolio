61a566bc3108c5b70287b256bec300efec0465b2
Salah AH <77612931+SalahAlHaismawi@users.noreply.github.com>
2024-07-16 16:26:47 +0800

    t

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,65 +5,42 @@ import Hero from '../../components/Hero';
 import Projects from '../../components/Projects';
 import Skills from '../../components/Skills';
 import ContactMe from '../../components/ContactMe';
-import Link from 'next/link';
-import MachineLearningSkills from "../../components/MachineLearningSkills";
-import ExperienceCard from "../../components/ExperienceCard";
-
-
-
+import MachineLearningSkills from '../../components/MachineLearningSkills';
 
 export default function Home() {
   return (
-          <div
-              className='bg-[#0b0c10] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#66fcf1]/80'>
-              <Head>
-                  <title>Salah portfolio</title>
-              </Head>
+    <div
+      className='bg-[#0b0c10] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#66fcf1]/80'
+      style={{ height: 'calc(var(--vh, 1vh) * 100)' }}
+    >
+      <Head>
+        <title>Salah portfolio</title>
+      </Head>
 
-              <Header/>
+      <Header />
 
+      <section id="hero" className='snap-center snap-mandatory'>
+        <Hero />
+      </section>
 
-              <section id="hero" className='snap-center snap-mandatory'>
+      <section id="about" className='snap-center'>
+        <About />
+      </section>
 
-                  <Hero/>
-              </section>
+      <section id='skills' className='snap-start'>
+        <Skills />
+      </section>
+      <section id='machinelearningskills' className='snap-start'>
+        <MachineLearningSkills />
+      </section>
 
-              {/* About */}
-              <section id="about" className='snap-center'>
+      <section id='projects' className='snap-start'>
+        <Projects />
+      </section>
 
-                  <About/>
-              </section>
-
-
-
-              {/*skills */}
-              {<section id='skills' className='snap-start'>
-                  <Skills/>
-              </section>}
-              {<section id='machinelearningskills' className='snap-start'>
-                  <MachineLearningSkills/>
-              </section>}
-              {/* Projects */}
-              <section id='projects' className='snap-start'>
-                  <Projects/>
-              </section>
-              {/* Contact me */}
-              <section id='contact' className='snap-start'>
-
-                  <ContactMe/>
-              </section>
-              {/*<Link href="#hero">*/}
-              {/*    <footer className='sticky bottom-20 w-full cursor-pointer'>*/}
-              {/*        <div className='flex items-center justify-center'>*/}
-              {/*            <img*/}
-              {/*                className="h-10 w-10 rounded-full hover:grayscale-1 hover:scale-150 transform transition duration-300 ease-in-out cursor-pointer"*/}
-              {/*                src="https://cdn-icons-png.flaticon.com/512/5973/5973800.png"*/}
-              {/*            />*/}
-
-              {/*        </div>*/}
-              {/*    </footer>*/}
-              {/*</Link>*/}
-          </div>
-
+      <section id='contact' className='snap-start'>
+        <ContactMe />
+      </section>
+    </div>
   );
-};
+}
